a49e24481b10eae17ca42a30b89d89ce7e018e4c
James Oshomah <jamiebones2000@yahoo.co.uk>
2022-12-18 10:30:56 +0100

    created the AI model and the addMessage and addMessages mutation

---
+++ server/graphql/resolvers/mutation/aiMutation.js
@@ -0,0 +1,79 @@
+const { AI } = require("../../../models/aiModel");
+const { ApolloError } = require("apollo-server-express");
+const { forEachDefaultValue } = require("@graphql-tools/utils");
+
+module.exports = {
+  addMessage: async (parent, args, context, info) => {
+    const { creator, mentioned, message } = args.fields;
+    console.log("Mutation > addMessage > args.fields = ", args.fields);
+    try {
+      if (!creator)
+        throw new ApolloError("The creator of the message is required.");
+      if (!mentioned) throw new ApolloError("The mentioned array is required");
+      if (mentioned && mentioned.length === 0)
+        throw new ApolloError(
+          "The mentioned array length must be greater than 0"
+        );
+      if (!message) throw new ApolloError("The message is required");
+
+      let fields = {
+        createdAt: new Date(),
+      };
+
+      fields.message = message;
+      fields.creator = creator;
+      fields.mentioned = mentioned;
+
+      const newAIData = await new AI(fields);
+
+      newAIData.save();
+      console.log("new ai data : ", newAIData);
+      return newAIData;
+    } catch (err) {
+      throw new ApolloError(err.message, err.extensions?.code || "addMessage", {
+        component: "aiMutation > addMessage",
+      });
+    }
+  },
+  addMessages: async (parent, args, context, info) => {
+    const { data } = args.fields;
+    console.log("Mutation > addMessages > args.fields = ", args.fields);
+    try {
+      if (!data || !data.length || data.length === 0)
+        throw new ApolloError("The array of messages is required");
+      let validData = [];
+      data.forEach((addMessageInputData) => {
+        const { creator, mentioned, message } = addMessageInputData;
+        let valid = false;
+        if (
+          creator &&
+          mentioned &&
+          mentioned.length &&
+          mentioned.length > 0 &&
+          message
+        ) {
+          valid = true;
+        }
+
+        if (valid) {
+          addMessageInputData.createdAt = new Date();
+          validData.push(addMessageInputData);
+        }
+      });
+
+      if (validData.length === 0)
+        throw new ApolloError("The passed data were not valid");
+
+      console.log("valid data ", validData)
+
+      //multiple insert
+      const newAIDataArray = await AI.insertMany(validData);
+      console.log("new ai data : ", newAIDataArray);
+      return newAIDataArray;
+    } catch (err) {
+      throw new ApolloError(err.message, err.extensions?.code || "addMessages", {
+        component: "aiMutation > addMessages",
+      });
+    }
+  },
+};
---
+++ server/models/aiModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const aiSchema = mongoose.Schema({
+  creator: String,
+  mentioned: [String],
+  message: String,
+  edenAI: {
+    keywords: [
+      {
+        keywords: String,
+        embedding: [mongoose.Decimal128],
+      },
+    ],
+    nodes: [mongoose.Schema.ObjectId],
+  },
+
+  createdAt: Date,
+});
+
+const AI = mongoose.model("AI", aiSchema);
+module.exports = { AI };
