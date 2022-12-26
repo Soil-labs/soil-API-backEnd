22ed53d146f9fe7d59a4691c032db6809d246bde
e-roy <ericroupe@gmail.com>
2022-12-26 04:02:45 -0500

    feat: token auth for eden frontend

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const init = require("./init");
 const login = require("./login");
+const token = require("./token");
 
 const authRoutes = () => {
   const router = Router();
 
   router.post("/login", login);
   router.get("/init", init);
+  router.post("/token", token);
 
   return router;
 };
---
+++ server/auth/token.js
@@ -0,0 +1,57 @@
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const { Members } = require("../models/membersModel");
+const {
+  retrieveAndMergeServersUserIsIn,
+} = require("../utils/updateUserServersInDB");
+
+const token = async ({ body }, res) => {
+  try {
+    const { access_token } = body;
+
+    if (!access_token) throw new Error("Invalid Token supplied");
+    const authResponse = await axios
+      .get(`https://discord.com/api/oauth2/@me`, {
+        headers: {
+          authorization: `Bearer ${access_token}`,
+        },
+      })
+      .catch((err) => {
+        throw new Error("Failed to get user");
+      });
+
+    let { user } = authResponse?.data;
+
+    // Find if user is in database
+    let dbUser = await Members.findOne({ _id: user.id });
+    // console.log("user", user);
+
+    // if user is not in database, save user to database
+    if (!dbUser) {
+      let fields = {
+        _id: user.id,
+        discordName: user.name,
+        avatar: user.avatar,
+        discriminator: user.discriminator,
+        registeredAt: new Date(),
+      };
+      dbUser = await new Members(fields);
+    }
+
+    await retrieveAndMergeServersUserIsIn(access_token, dbUser);
+
+    const token = jwt.sign(
+      { _id: user.id, discordName: user.username },
+      process.env.JWT_SECRET || "",
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.json({ eden_token: token });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+module.exports = token;
