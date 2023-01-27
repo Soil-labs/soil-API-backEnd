eaed69612c4e814fc69174d2e6f89bafc6f4e10f
James Oshomah <jamiebones2000@yahoo.co.uk>
2023-01-27 07:23:11 +0100

    feature to view one member and other members graph

--- server/graphql/resolvers/query/graphVisualQuery.js
+++ server/graphql/resolvers/query/graphVisualQuery.js
@@ -356,6 +356,91 @@ module.exports = {
         }
       );
     }
+  },
+  findOneMemberToMembersGraph: async (parent, args, context, info) => {
+    const { memberID } = args.fields;
+    console.log("Query > findOneMemberToMembersGraph > args.fields = ", args.fields);
 
+    if (!memberID) throw new ApolloError("The memberID is required");
+
+    try {
+      let memberData = await Members.find({ _id: memberID }).select(
+        "_id"
+      );
+
+
+      if (!memberData) throw new ApolloError("Member data not found");
+
+      console.log("memberData = ", memberData);
+
+      res = await generalFunc_neo4j({
+        request: `
+        MATCH res = ((m)-[]-()-[]-()-[]-(o:Member)) 
+         WHERE m._id = "${memberID}"
+         RETURN res
+        `,
+      });
+
+      nodesObj = {};
+      edgesArr = [];
+      
+      for (let i = 0; i < res.records.length; i++) {
+        let record = res.records[i];
+
+        for (let j = 0; j < record._fields[0].segments.length; j++) {
+          let segment = record._fields[0].segments[j];
+
+          let start = segment.start;
+          let end = segment.end;
+          if (nodesObj[start.properties._id] == undefined) {
+            nodesObj[start.properties._id] = {
+              _id: start.properties._id,
+              name: start.properties.name,
+              type: start.labels[0],
+            };
+          }
+          if (nodesObj[end.properties._id] == undefined) {
+            nodesObj[end.properties._id] = {
+              _id: end.properties._id,
+              name: end.properties.name,
+              type: end.labels[0],
+            };
+          }
+
+          edgesArr.push({
+            source: start.properties._id,
+            target: end.properties._id,
+            type: segment.relationship.type,
+          });
+        }
+      }
+
+      let nodesArr = [];
+      for (let key in nodesObj) {
+        nodesArr.push({
+          _id: nodesObj[key]._id,
+          name: nodesObj[key].name,
+          type: nodesObj[key].type,
+        });
+      }
+
+      //console.log("nodesObj = ", nodesObj);
+      //console.log("edgesArr = ", edgesArr);
+
+      const uniqueEdges = _.uniqWith(edgesArr, _.isEqual);
+
+      return {
+        nodes: nodesArr,
+        edges: uniqueEdges,
+      };
+    } catch (err) {
+      throw new ApolloError(
+        err.message,
+        err.extensions?.code || "findOneMemberToMembersGraph",
+        {
+          component: "graphVisual > findOneMemberToMembersGraph",
+        }
+      );
+    }
   }
 };
