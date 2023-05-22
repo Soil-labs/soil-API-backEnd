8dafb577654b17745d9840aed40c7a956f3365e4
heavensbee <heavensbee001@gmail.com>
2023-05-22 15:55:42 +0200

    Fix company model

--- server/models/companyModel.js
+++ server/models/companyModel.js
@@ -1,72 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-
 const companyModel = mongoose.Schema({
   name: String,
-  employees: [{
-    typeT: String,
-    userID: String,
-  }],
-  questionsToAsk:[{
-    questionID: mongoose.Schema.ObjectId,
-    bestAnswer: String,
-  }],
-  nodes: [{
-    nodeID: mongoose.Schema.ObjectId,
-  }],
-  convRecruiter: [{
-    userID: String,
-    conversationID: mongoose.Schema.ObjectId,
-    readyToDisplay: Boolean,
-    companyQuestions: [{
-      question: String,
-      content: String,
-    }],
-    roleQuestions: [{
-      question: String,
-      content: String,
-    }],
-    convMemory: [{
-      memoryContent: String,
-      pineConeID: String,
-    }],
-  }],
-  convRecruiterReadyToDisplay: Boolean,
-  talentList: [{
-    name: String,
-    talent: [{
+  employees: [
+    {
+      typeT: String,
       userID: String,
-    }]
-  }],
-  candidatesReadyToDisplay: Boolean,
-  candidates: [{
-    userID: String,
-    overallScore: Number,
-    acceptedOrRejected: Boolean,
-    readyToDisplay: Boolean,
-    conversationID: mongoose.Schema.ObjectId,
-    summaryQuestions: [{
-      questionID: mongoose.Schema.ObjectId,
-      questionContent: String,
-      questionContentSmall: String,
-      answerContent: String,
-      answerContentSmall: String,
-      bestAnswerCompany: String,
-      reason: String,
-      score: Number,
-      subConversationAnswer: [{
-        role: String, // user or bot
-        content: String,
-      }],
-    }],
-    interviewQuestionsForCandidate: [{
-      originalQuestionID: mongoose.Schema.ObjectId,
-      originalContent: String,
-      personalizedContent: String,
-    }],
-  }],
-
+    },
+  ],
+  url: String,
+  positions: [mongoose.Schema.ObjectId],
 });
 
 const Company = mongoose.model("Company", companyModel);
