1ed61fa5805a5887a1542799b6963380d8cde101
BluePanda <m.saratzidis@gmail.com>
2023-05-22 20:08:27 +0200

    fix problem

--- server/graphql/resolvers/mutation/conversationMutation.js
+++ server/graphql/resolvers/mutation/conversationMutation.js
@@ -97,8 +97,9 @@ module.exports = {
 
           console.log("userID,positionID = " , userID,positionID)
 
-          // CandidateNotesEdenAIAPICallF(userID,companyID)
-          // df0
+          if (userID!=undefined && positionID!=undefined) 
+            CandidateNotesEdenAIAPICallF(userID,positionID)
+          df0
 
 
           // --------------- Calculate candidateNotesEdenAI ---------------
