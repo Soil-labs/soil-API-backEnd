58f2c705e77a5bfa96b44353ccc7455053c3d214
BluePanda <m.saratzidis@gmail.com>
2023-05-23 20:58:11 +0200

    I think that everything is here hmmm

--- server/graphql/resolvers/mutation/positionMutation.js
+++ server/graphql/resolvers/mutation/positionMutation.js
@@ -572,6 +572,48 @@ module.exports = {
         );
       }
     },
+    deletePositionCandidate: async (parent, args, context, info) => {
+      const { positionID, userID } = args.fields;
+      console.log(
+        "Query > deletePositionCandidate > args.fields = ",
+        args.fields
+      );
+  
+      if (!positionID) throw new ApolloError("positionID is required");
+  
+      if (!userID) throw new ApolloError("userID is required");
+  
+      try {
+        let positionData = await Position.findOne({ _id: positionID }).select(
+          "_id candidates"
+        );
+  
+        if (!positionData) throw new ApolloError("Position not found");
+  
+        console.log("positionData = ", positionData);
+  
+        // find the positionData.candidates userID and return it
+  
+        // const candidate = positionData.candidates.find(candidate => candidate.userID.toString() == userID.toString());
+        const index = positionData.candidates.findIndex(
+          (candidate) => candidate.userID.toString() == userID.toString()
+        );
+  
+        positionData.candidates[index].interviewQuestionsForCandidate = [];
+  
+        positionData = await positionData.save();
+  
+        // console.log("candidate = " , candidate)
+  
+        return positionData.candidates[index];
+      } catch (err) {
+        throw new ApolloError(
+          err.message,
+          err.extensions?.code || "deletePositionCandidate",
+          { component: "positionQuery > deletePositionCandidate" }
+        );
+      }
+    },
     addConvRecruiterToPosition: async (parent, args, context, info) => {
       const { positionID,userID,conversationID } = args.fields;
       console.log("Mutation > addConvRecruiterToPosition > args.fields = ", args.fields);
