89f1e7921f8b4c29bb9afec5d7bb5df64dffe24e
BluePanda <m.saratzidis@gmail.com>
2023-05-24 21:42:03 +0200

    f

--- server/graphql/resolvers/mutation/aiMutation.js
+++ server/graphql/resolvers/mutation/aiMutation.js
@@ -334,7 +334,7 @@ module.exports = {
   },
   saveCVtoUser: async (parent, args, context, info) => {
     const { cvContent, userID, positionID } = args.fields;
-    console.log("Mutation > saveCVtoUser > args.fields = ", args.fields);
+    // console.log("Mutation > saveCVtoUser > args.fields = ", args.fields);
 
     if (!userID) {
       throw new ApolloError("userID is required");
@@ -393,9 +393,12 @@ module.exports = {
         Main Skills 3 words max:
         Summary 3 sentenses max:: 
       `
-      printC(cvContentPrompt,"3","cvContentPrompt","b")
+      // printC(cvContentPrompt,"3","cvContentPrompt","b")
 
       titleSkillSummaryRes = await useGPTchatSimple(cvContentPrompt,0,"API 2")
+
+
+      printC(titleSkillSummaryRes,"3","titleSkillSummaryRes","b")
 
       const titleRole = titleSkillSummaryRes.match(/Title Role: (.*)/)[1];
       const mainSkills = titleSkillSummaryRes.match(/Main Skills: (.*)/)[1].split(", ");
