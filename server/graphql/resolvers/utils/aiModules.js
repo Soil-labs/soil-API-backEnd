bb420423630a4dc55cd6db200a806a1724082176
BluePanda <m.saratzidis@gmail.com>
2023-06-20 12:41:47 +0200

    find priorities trainEdenAI page

--- server/graphql/resolvers/utils/aiModules.js
+++ server/graphql/resolvers/utils/aiModules.js
@@ -135,7 +135,7 @@ async function positionTextAndConvoToReportCriteriaFunc(positionID) {
   }
 
   positionsRequirements = positionData.positionsRequirements.originalContent;
-  // printC(positionsRequirements,"0","positionsRequirements","b")
+  // printC(positionData,"0","positionData","b")
 
   positionsRequirements = positionsRequirements.replace(/b\d+:\s/g, "");
   printC(positionsRequirements, "0.5", "positionsRequirements", "b");
