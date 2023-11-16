b959419f8291ca9e2cc85aed805d20ca2d59ffec
Miltiadis Saratzidis <miltossaratzidis1@gmail.com>
2023-11-16 09:02:58 -0500

    hideGPTkey

--- server/graphql/resolvers/utils/memoryPineconeModules.js
+++ server/graphql/resolvers/utils/memoryPineconeModules.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const { Node } = require("../../../models/nodeModal");
 const { Members } = require("../../../models/membersModel");
 const { Position } = require("../../../models/positionModel");
@@ -16,19 +18,13 @@ const { REACT_APP_MONGO_DATABASE } = process.env;
 
 
 function chooseAPIkey(chooseAPI = "") {
-  // openAI_keys = [
-  //   "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",
-  //   // "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",
-  //   "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",
-  //   "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",
-  // ];
 
-  let openAI_keys = ["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"];
+  let openAI_keys = [process.env.REACT_APP_OPENAI_1];
 
   if (chooseAPI == "API 2") {
-    openAI_keys = ["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"];
+    openAI_keys = [process.env.REACT_APP_OPENAI_2];
   } else if (chooseAPI == "API 1") {
-    openAI_keys = ["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"];
+    openAI_keys = [process.env.REACT_APP_OPENAI_1];
   }
 
   // randomly choose one of the keys
--- server/graphql/resolvers/utils/questionsEdenAIModules.js
+++ server/graphql/resolvers/utils/questionsEdenAIModules.js
@@ -1,4 +1,6 @@
 
+require("dotenv").config();
+
 const { QuestionsEdenAI } = require("../../../models/questionsEdenAIModel");
 
 const axios = require("axios");
@@ -13,19 +15,13 @@ const {
 } = require("../utils/memoryPineconeModules");
 
 function chooseAPIkey(chooseAPI = "") {
-  // openAI_keys = [
-  //   "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",
-  //   // "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",
-  //   "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",
-  //   "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",
-  // ];
 
-  let openAI_keys = ["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"];
+  let openAI_keys = [process.env.REACT_APP_OPENAI_1];
 
   if (chooseAPI == "API 2") {
-    openAI_keys = ["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"];
+    openAI_keys = [process.env.REACT_APP_OPENAI_2];
   } else if (chooseAPI == "API 1") {
-    openAI_keys = ["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"];
+    openAI_keys = [process.env.REACT_APP_OPENAI_1];
   }
 
   // randomly choose one of the keys
