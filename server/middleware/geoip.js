e68f3abb29c13888955146101b9f57723862fd34
heavensbee <heavensbee001@gmail.com>
2024-01-12 15:53:23 +0100

    Fix merge

--- server/middleware/geoip.js
+++ server/middleware/geoip.js
@@ -1,17 +1,17 @@
-// const express = require("express");
-// const geoip = require("geoip-lite");
-// const app = express();
+const express = require("express");
+const geoip = require("geoip-lite");
+const app = express();
 
-// const originAuth = (req, res, next) => {
-//   const ip = req.ip.includes("::ffff:") ? req.ip.split("::ffff:")[1] : req.ip;
-//   const geo = geoip.lookup(ip);
-//   const host = req.headers.host || "";
-//   const allowedDomain = "edeprotocol.app";
-//   const developDomain = "eden-saas-develop.vercel.app";
+const originAuth = (req, res, next) => {
+  const ip = req.ip.includes("::ffff:") ? req.ip.split("::ffff:")[1] : req.ip;
+  const geo = geoip.lookup(ip);
+  const host = req.headers.host || "";
+  const allowedDomain = "edeprotocol.app";
+  const developDomain = "eden-saas-develop.vercel.app";
 
-//   //   console.log("IP", ip);
-//   //   console.log("GEO", geo);
-//   //   console.log("Region", geo.region);
+  //   console.log("IP", ip);
+  //   console.log("GEO", geo);
+  //   console.log("Region", geo.region);
 
   console.log("middleware ======= ", referer);
   console.log("middleware ======= ", geo);
@@ -29,4 +29,4 @@
   }
 };
 
-// module.exports = originAuth;
+module.exports = originAuth;
